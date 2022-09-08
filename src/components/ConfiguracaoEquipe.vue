54d4c9af5529328456fc86076c0b75a89ed091ec
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-09-08 14:12:28 -0300

    MapState

--- src/components/ConfiguracaoEquipe.vue
+++ src/components/ConfiguracaoEquipe.vue
@@ -7,12 +7,12 @@
     </div>
     <div class="row">
       <div class="col-8">
-        <p>Enfermeiro:</p>
-        <p>Socorrista:</p>
-        <p>Médico:</p>
-        <p>Carro:</p>
-        <p>Telefone:</p>
-        <p>Kit de reanimação:</p>
+        <p>Enfermeiro:{{equipe.enfermeiro}}</p>
+        <p>Socorrista: {{equipe.socorrista}}</p>
+        <p>Médico: {{equipe.medico}} </p>
+        <p>Carro: {{equipe.carro}} </p>
+        <p>Telefone: {{equipe.telefone}}</p>
+        <p>Kit de reanimação: {{equipe.kitDeReanimacao}}</p>
       </div>
       <div class="col-4 text-center">
         <div class="row">
@@ -31,7 +31,9 @@
 </template>
 
 <script>
+  import {mapState} from 'vuex'
 export default {
-    name: 'ConfiguracaoEquipe'
+    name: 'ConfiguracaoEquipe',
+    computed: mapState(['equipe'])
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -3,6 +3,13 @@ import Vuex from 'vuex'
 export default new Vuex.Store({
     state: {
         titulo:'Emergências Médicas',
-        titulo2:'Equipes de Emergência'
+        equipe: {
+            enfermeiro: 'Nome do enfermeiro',
+            socorrista: 'Nome do socorrista',
+            medico: 'Nome do medico',
+            carro: 'Placa do carro',
+            telefone: '+55 11 91117-5506',
+            kitDeReanimacao: 'Kit 0001'
+        }
     }
 })
