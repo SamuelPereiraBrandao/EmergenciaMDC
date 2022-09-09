860a1d7b28b48aab2f7a4a2d28714cfeb6126044
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-09-09 18:32:58 -0300

    finish

--- src/components/Equipe.vue
+++ src/components/Equipe.vue
@@ -1,6 +1,14 @@
 <template>
-    <div>
-        {{dados}}
+  
+    <div class="row">
+
+    
+        <div class="col-2" v-text="dados.enfermeiro"></div>
+        <div class="col-2" v-text="dados.socorrista"></div>
+        <div class="col-2" v-text="dados.medico"></div>
+        <div class="col-2" v-text="dados.carro"></div>
+        <div class="col-2" v-text="dados.telefone"></div>
+        <div class="col-2" v-text="dados.kitDeReanimacao"></div>
     </div>
 </template>
 
--- src/components/Equipes.vue
+++ src/components/Equipes.vue
@@ -8,8 +8,20 @@
                 </h5>
             </div>
         </div>
+        
         <div class="row">
+            
             <div class="col">
+                
+                <div class="row borda">
+                    
+                    <div class="col-2"><h6>Enfermeiro</h6> </div>
+                    <div class="col-2"> <h6>Socorrista</h6></div>
+                    <div class="col-2"><h6>Medico</h6></div>
+                    <div class="col-2"><h6>Carro</h6></div>
+                    <div class="col-2"><h6>Telefone</h6></div>
+                    <div class="col-2"><h6>Kit</h6></div>
+                </div>
                 <ListaEquipes tipo="equipes-montadas" />
             </div>
         </div>
@@ -26,3 +38,10 @@ export default {
 }
 }
 </script>
+<style scoped>
+    .borda{
+        border: solid 1px rgb(190, 190, 190);
+        border-radius: 5px;
+
+    }
+</style>
